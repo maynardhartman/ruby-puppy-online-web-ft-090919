5eec81748b2ae8cac060c19380c412337ccf28e6
Maynard Hartman <mjhartman@brighthouse.com>
2019-10-07 17:40:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Add your code here
+class Dog 
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @@all.each { |dog|
+      puts "#{dog}"
+    }
+  end
